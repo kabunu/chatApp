12c468223ee56ef0015bb8d8b0173b0797948f13
aminu kabunu <aminu.kabunu@gmail.com>
2020-05-17 13:07:15 +0100

    update background

--- src/components/TextContainer/TextContainer.js
+++ src/components/TextContainer/TextContainer.js
@@ -7,15 +7,15 @@ import './TextContainer.css';
 const TextContainer = ({ users }) => (
   <div className="textContainer">
     <div>
-      <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
-      <h2>Created with React, Express, Node and Socket.IO <span role="img" aria-label="emoji">❤️</span></h2>
-      <h2>Try it out right now! <span role="img" aria-label="emoji">⬅️</span></h2>
+      <h1>Welcome to Ctech Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
+      <h2>Created with  React js, Express, Node js and Socket.IO <span role="img" aria-label="emoji">❤️</span></h2>
+      <h2>Lets get chatty! <span role="img" aria-label="emoji">⬅️</span></h2>
     </div>
     {
       users
         ? (
           <div>
-            <h1>People currently chatting:</h1>
+            <h1>Users online chatting:</h1>
             <div className="activeContainer">
               <h2>
                 {users.map(({name}) => (
